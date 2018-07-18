38bb8dcbbb0b4838636eef75b90d9929ba253bef
Tuan Anh (Marcus) <tuananh6109@gmail.com>
2018-07-18 10:43:47 +0700

    Init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as web3 from 'web3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  docAddress: FormControl = new FormControl();
+  verifierAddress: FormControl = new FormControl();
+  documentProof: string;
+  generatedString: string;
+  canDisplay: boolean;
+
+  constructor() {
+    this.docAddress.valueChanges.subscribe(
+      val => {
+        if (!this.isHexValue(val)) {
+          this.docAddress.setErrors({message: 'Invalid Dac address. Example: 0x213lkjkfsadf'});
+        }
+      }
+    );
+
+    this.verifierAddress.valueChanges.subscribe(
+      val => {
+        if (!this.isHexValue(val)) {
+          this.verifierAddress.setErrors({message: 'Invalid Verifier address. Example: 0x213lkjkfsadf'});
+        }
+      }
+    );
+  }
+
+  generateSig() {
+    this.generatedString = this.docAddress.value + this.verifierAddress.value + this.documentProof;
+  }
+
+  copyToClipboard() {
+    document.querySelector('textarea').select();
+    document.execCommand('copy');
+  }
+
+  isHexValue(value: string): boolean {
+    const regExp = new RegExp(/0[xX][0-9a-fA-F]+/);
+    return regExp.test(value);
+  }
+}
